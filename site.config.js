3fab550c9669e08c9b62f2b1cfa74a0275427036
babybearofficial <60312140+babybearofficial@users.noreply.github.com>
2024-01-02 15:07:17 +0800

    Update site.config.js instead of former account.

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '6a76fdd9ec6d4ae2855aa790d620eec4',
+  rootNotionPageId: 'd75ddca35fc6449ba7b8dc041f106f61',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
